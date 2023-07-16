8967524b81113a3698f61b09b6f84bb00f926994
Yousef360 <95775013+YousefTB@users.noreply.github.com>
2023-07-17 01:07:18 +0300

    Add files via upload

---
+++ main_test.py
@@ -0,0 +1,155 @@
+import sys
+import pytest
+from PySide2.QtWidgets import QApplication
+from PySide2 import QtCore
+from python_plotter import MainWindow
+
+
+@pytest.fixture
+def gui(qtbot):
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+        
+    window = MainWindow()
+    qtbot.addWidget(window)
+    qtbot.wait_for_window_shown(window)
+    yield window
+    window.close()
+    
+
+def test_plot_linear_function(gui, qtbot):
+    # Test plot output for a simple linear function
+    qtbot.keyClicks(gui.function_input, '2*x + 1')
+    qtbot.keyClicks(gui.xmin_input, '-5')
+    qtbot.keyClicks(gui.xmax_input, '5')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+
+def test_input_validation_invalid_function(gui, qtbot):
+    # Test invalid function input
+    qtbot.keyClicks(gui.function_input, '5x**z')
+    qtbot.keyClicks(gui.xmin_input, '0')
+    qtbot.keyClicks(gui.xmax_input, '1')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == 'Error: Invalid Function'
+    assert not gui.canvas.figure.gca().has_data()
+    
+
+def test_input_validation_non_numeric_xmin_xmax(gui, qtbot):
+    # Test invalid xmin and xmax inputs
+    qtbot.keyClicks(gui.function_input, 'x^2')
+    qtbot.keyClicks(gui.xmin_input, 'a')
+    qtbot.keyClicks(gui.xmax_input, 'b')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == 'Error: xmin and xmax must be numbers'
+    assert not gui.canvas.figure.gca().has_data()
+    
+
+def test_plot_validation_xmin_is_greater(gui, qtbot):
+    # Test invalid xmin and xmax inputs that is xmin is greater
+    qtbot.keyClicks(gui.function_input, 'x^2')
+    qtbot.keyClicks(gui.xmin_input, '20')
+    qtbot.keyClicks(gui.xmax_input, '-12')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == 'Error: xmin is greater or equal to xmax'
+    assert not gui.canvas.figure.gca().has_data()
+    
+
+def test_decimal_input_values(gui, qtbot):
+    # Test plot output for input values with decimal places
+    qtbot.keyClicks(gui.function_input, '2^x')
+    qtbot.keyClicks(gui.xmin_input, '-1.5')
+    qtbot.keyClicks(gui.xmax_input, '1.5')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+
+def test_large_input_values(gui, qtbot):
+    # Test plot output for a large range of input values
+    qtbot.keyClicks(gui.function_input, 'x^3 + 4*x^2 + 3*x + 2')
+    qtbot.keyClicks(gui.xmin_input, '-100000')
+    qtbot.keyClicks(gui.xmax_input, '100000')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+
+def test_zero_division(gui, qtbot):
+    # Test function that contains division on zero
+    qtbot.keyClicks(gui.function_input, 'x + 21 ^ x / 0')
+    qtbot.keyClicks(gui.xmin_input, '21')
+    qtbot.keyClicks(gui.xmax_input, '39')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+def test_complex_function(gui, qtbot):
+    # Test complex function
+    qtbot.keyClicks(gui.function_input, '(x ^ 3 / 10 ^ x) + 49 * x - x ^ 2 + (x ^ (1/2))')
+    qtbot.keyClicks(gui.xmin_input, '-1395')
+    qtbot.keyClicks(gui.xmax_input, '9321')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+
+
+def test_mix_of_invalids(gui, qtbot):
+    # Test mix of invalid input xmin and xmax and invalid function
+    qtbot.keyClicks(gui.function_input, 'x+23/dmcxzlqls.fg')
+    qtbot.keyClicks(gui.xmin_input, 'abcdj1i3k499293949l')
+    qtbot.keyClicks(gui.xmax_input, 'yoamzl12945i3edfjjdsf')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == 'Error: xmin and xmax must be numbers'
+    assert not gui.canvas.figure.gca().has_data()
+    
+def test_toy_function1(gui, qtbot):
+    qtbot.keyClicks(gui.function_input, 'x^-10/(x*21^2)')
+    qtbot.keyClicks(gui.xmin_input, '-2139490')
+    qtbot.keyClicks(gui.xmax_input, '2339911')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+def test_toy_function2(gui, qtbot):
+    qtbot.keyClicks(gui.function_input, '(x/19)^(2*x)')
+    qtbot.keyClicks(gui.xmin_input, '-5')
+    qtbot.keyClicks(gui.xmax_input, '92129')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+def test_toy_function3(gui, qtbot):
+    qtbot.keyClicks(gui.function_input, '102')
+    qtbot.keyClicks(gui.xmin_input, '-9129049')
+    qtbot.keyClicks(gui.xmax_input, '3993955869921')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+def test_toy_function4(gui, qtbot):
+    qtbot.keyClicks(gui.function_input, 'x^9/((1/x)^-2)')
+    qtbot.keyClicks(gui.xmin_input, '93')
+    qtbot.keyClicks(gui.xmax_input, '552')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+def test_toy_function5(gui, qtbot):
+    qtbot.keyClicks(gui.function_input, '5/(9-(x^3))')
+    qtbot.keyClicks(gui.xmin_input, '93')
+    qtbot.keyClicks(gui.xmax_input, '552')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
+    
+def test_toy_function5(gui, qtbot):
+    qtbot.keyClicks(gui.function_input, '(29 - (x - 4)^2)^(1/2) - 3')
+    qtbot.keyClicks(gui.xmin_input, '-10')
+    qtbot.keyClicks(gui.xmax_input, '10')
+    qtbot.mouseClick(gui.plot_button, QtCore.Qt.LeftButton)
+    assert gui.statusBar().currentMessage() == ''
+    assert gui.canvas.figure.gca().has_data()
---
+++ python_plotter.py
@@ -0,0 +1,133 @@
+import sys
+import numpy as np
+from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+
+#=====================================================================================
+#MainWindow Class is used to make an object of QMainWindow to create full GUI Window #
+#Customized based on the required task                                               #
+#Instance of this class creates MainWindow object that used to build the GUI         #
+#=====================================================================================
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        # Window configuration
+        self.setWindowTitle("Python Plotter")
+        self.resize(800, 600)
+
+        # Create widgets
+        self.function_label = QLabel('Enter function:')
+        self.function_input = QLineEdit()
+        self.xmin_label = QLabel('Enter minimum value of x:')
+        self.xmin_input = QLineEdit()
+        self.xmax_label = QLabel('Enter maximum value of x:')
+        self.xmax_input = QLineEdit()
+        self.plot_button = QPushButton('Plot')
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        # Set layout
+        self.central_widget = QWidget()
+        self.central_widget.setLayout(self.create_layout())
+        self.setCentralWidget(self.central_widget)
+
+        # Connect button signal to slot
+        self.plot_button.clicked.connect(self.plot)
+
+    def create_layout(self):
+        layout = QVBoxLayout()
+        layout.addWidget(self.function_label)
+        layout.addWidget(self.function_input)
+        layout.addWidget(self.xmin_label)
+        layout.addWidget(self.xmin_input)
+        layout.addWidget(self.xmax_label)
+        layout.addWidget(self.xmax_input)
+        layout.addWidget(self.plot_button)
+        layout.addWidget(self.canvas)
+        return layout
+
+    def plot(self):
+        # Get user input
+        function = self.function_input.text()
+        xmin = self.xmin_input.text()
+        xmax = self.xmax_input.text()
+
+        # Validate input
+        try:
+            xmin = float(xmin)
+            xmax = float(xmax)
+        except ValueError:
+            self.canvas.figure.clear()
+            self.canvas.draw()
+            self.statusBar().showMessage('Error: xmin and xmax must be numbers')
+            return
+        
+        if xmin >= xmax:
+            self.canvas.figure.clear()
+            self.canvas.draw()
+            self.statusBar().showMessage('Error: xmin is greater or equal to xmax')
+            return
+        
+        
+        # Create x values
+        n_points = round(abs(xmin) + xmax + 1)
+        if n_points > 100000:
+            n_points = 100000
+        
+        x = np.linspace(xmin, xmax, n_points)
+        
+        # add 0 point to x to enhance the output shape
+        if xmin < 0:
+            x = np.append(x,0)
+            x = np.sort(x)
+
+        # Evaluate function
+        try:
+            y = eval(function.replace('^', '**')) # X variable is defined in line 68 to be evaluated
+        except:
+            self.canvas.figure.clear()
+            self.canvas.draw()
+            self.statusBar().showMessage('Error: Invalid Function')
+            return
+        
+
+        # Normalize all np.inf values to np.nan if exist
+        try:
+            y[y == np.inf] = np.nan
+        except:
+            pass
+
+        # Clear figure and plot
+        self.canvas.figure.clear()
+        self.statusBar().showMessage("")
+        ax = self.figure.add_subplot(111)
+        
+        # Handling the constant functions
+        try:
+            ax.plot(x, y)
+        except:
+            y = np.full(x.shape, y)
+            ax.plot(x, y)
+            
+        ax.set_xlabel('x-axis')
+        ax.set_ylabel('y-axis')
+        ax.set_title('Plot of ' + function)
+        self.canvas.draw()
+
+
+if __name__ == '__main__':
+    # Making sure there is no instance (Since using spyder)
+    if not QApplication.instance():
+        app = QApplication(sys.argv)
+    else:
+        app = QApplication.instance()
+    
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
